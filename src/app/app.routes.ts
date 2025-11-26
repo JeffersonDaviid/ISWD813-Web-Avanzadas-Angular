94b12772d77b68121277fc8f537489442063951e
Jefferson Chileno <jefferson.chileno@epn.edu.ec>
2025-11-26 16:42:09 -0500

    feat: examen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Acerca } from './components/acerca/acerca';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'acerca',
+    component: Acerca,
+  },
+];
